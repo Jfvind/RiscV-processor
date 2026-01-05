db10ce6288ec99d0c454c576ff79c182fa0beadc
Jfvind <s245326@dtu.dk>
2026-01-05 15:14:21 +0100

    fix verilog refference

--- src/main/scala/Top.scala
+++ src/main/scala/Top.scala
@@ -1,12 +1,10 @@
-package core
-
 import chisel3._
 import circt.stage.ChiselStage
 
 object Top extends App {
   // This generates the Verilog file "Core.sv" (SystemVerilog)
   ChiselStage.emitSystemVerilogFile(
-    new Core(),
+    new core.Core(),
     args = Array("--target-dir", "generated")
   )
 }
--- src/main/scala/core/Core.scala
+++ src/main/scala/core/Core.scala
@@ -1,1 +1,9 @@
 // This is the "motherboard." It instantiates the ALU, Register File, and Control Unit and wires them together.
+
+package core
+
+import chisel3._
+
+class Core extends Module {
+    // bla bla
+}
