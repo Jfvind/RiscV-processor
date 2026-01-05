a897014239dffb768a284b5efd772bac71fed6ae
freddyTheAlch <s220768@dtu.dk>
2026-01-05 15:26:12 +0100

    RegInit from Schoerbl for debug purposes

--- src/main/scala/core/RegisterFile.scala
+++ src/main/scala/core/RegisterFile.scala
@@ -1,2 +1,4 @@
 // Contains the 32 general-purpose registers (x0-x31). It handles reading two registers and writing to one.
 import chisel3._
+// Contains the 32 general-purpose registers (x0-x31). It handles reading two registers and writing to one.
+(RegInit(VecInit(Seq.fill(32)(0.U(32.W)))))
