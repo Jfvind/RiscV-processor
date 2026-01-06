6825fca331b261a59cce51eac20afcdca753d4b8
freddyTheAlch <s220768@dtu.dk>
2026-01-06 10:58:22 +0100

    ALU test + I/O def of ALU

--- src/main/scala/core/ALU.scala
+++ src/main/scala/core/ALU.scala
@@ -7,4 +7,25 @@ import chisel3._
 Input A, Input B (efter forwarding MUXes!)
 ALUOp control signal
 Output: result, zero flag (til branches senere)
+
+OPCODE:
+0 -> ADD
+1 -> SUB
+2 -> AND
+3 -> OR
+4 -> XOR
+5 -> SLL  (shift left logical)
+6 -> SRL  (shift right logical)
+7 -> SRA  (shift right arithmetic)
+8 -> SLT  (set less than - signed)
+9 -> SLTU (set less than - unsigned)
  */
+
+class ALU extends Module {
+  val io = IO(new Bundle {
+    val op_a   = Input(UInt(32.W))
+    val op_b   = Input(UInt(32.W))
+    val alu_op = Input(UInt(4.W))
+    val result = Output(UInt(32.W))
+  })
+}
