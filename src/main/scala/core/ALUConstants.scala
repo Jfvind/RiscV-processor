9f3ea2ef30b140d63f4efce477826e17ae742101
freddyTheAlch <s220768@dtu.dk>
2026-01-14 18:40:30 +0100

    perf:(ALUConstants): Changed the isSub helper function to only consist of wires and not OR gates. This was done by taking a bit approach instead of decimal

--- src/main/scala/core/ALUConstants.scala
+++ src/main/scala/core/ALUConstants.scala
@@ -10,23 +10,22 @@ object ALUConstants {
   val OP_WIDTH = 4.W // Defining the width of the control signal to 4 bits. 
 
   // Opcode mappings:
-  // U(OP_WIDTH) for locking bit width.
-  val ALU_ADD  = 0.U(OP_WIDTH) 
-  val ALU_SUB  = 1.U(OP_WIDTH)
-  val ALU_AND  = 2.U(OP_WIDTH)
-  val ALU_OR   = 3.U(OP_WIDTH)
-  val ALU_XOR  = 4.U(OP_WIDTH)
-  val ALU_SLL  = 5.U(OP_WIDTH)
-  val ALU_SRL  = 6.U(OP_WIDTH)
-  val ALU_SRA  = 7.U(OP_WIDTH)
-  val ALU_SLT  = 8.U(OP_WIDTH)
-  val ALU_SLTU = 9.U(OP_WIDTH)
-  val ALU_LUI  = 10.U(OP_WIDTH)
+  // MSB (bit 3) = subtraction mode
+  val ALU_ADD  = "b0000".U  // 0
+  val ALU_AND  = "b0001".U  // 1
+  val ALU_OR   = "b0010".U  // 2
+  val ALU_XOR  = "b0011".U  // 3
+  val ALU_SLL  = "b0100".U  // 4
+  val ALU_SRL  = "b0101".U  // 5
+  val ALU_SRA  = "b0110".U  // 6
+  val ALU_LUI  = "b0111".U  // 7
 
-  // Helper function: Checks if the operation requires substra
-  // This is used to reduce area of the ALU utilizing boolean alg. IE: A + B and A - B can utilize same adder,
-  // as A - B = A + (B')
+  // Subtraction ops (bit 3 = 1)
+  val ALU_SUB  = "b1000".U  // 8
+  val ALU_SLT  = "b1100".U  // 12
+  val ALU_SLTU = "b1101".U  // 13
+
   def isSub(op: UInt): Bool = {
-    op === ALU_SUB || op === ALU_SLT || op === ALU_SLTU
+    op(3)  // Wiring with this approach, should be faster than with OR gates.
   }
 }
