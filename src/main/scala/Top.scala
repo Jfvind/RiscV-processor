20adc14915c7553c4ff756a0cbb8d95a850a8e16
Jfvind <s245326@dtu.dk>
2026-01-23 05:56:12 +0100

    Changed freq to 50MHz

--- src/main/scala/Top.scala
+++ src/main/scala/Top.scala
@@ -10,6 +10,25 @@ class Top extends Module {
     val tx  = Output(Bool())
   }) // The debug outputs from Core are left unconnected (ignored)
 
+  // --- CLOCK DIVIDER (Fixer -8ns slack problemet) ---
+  // Vi tæller op: 00 -> 01 -> 10 -> 11 ...
+  // Bit 1 (counter(1)) skifter hver 4. cyklus -> 100 MHz / 2 = 50 MHz
+  val counter = RegInit(0.U(1.W))
+  counter := counter + 1.U
+  
+  val slowClock = counter(0).asClock
+
+  // --- CORE INSTANTIATION ---
+  // Vi pakker Core ind i 'withClock', så den kører på 25 MHz
+  withClock(slowClock) {
+    val core = Module(new Core(programFile = "prime_bench.mem"))
+    
+    // Forbindelser
+    io.tx := core.io.tx
+    
+    io.led := core.io.led
+  }
+  /*
   // --- CORE INSTANTIATION ---
   // Otherwise we use a filepath: e.g. when running the prime_benchmark
   //val core = Module(new Core(programFile = "prime_bench.mem"))
@@ -18,7 +37,7 @@ class Top extends Module {
   io.led := core.io.led
 
   // --- UART INSTANTIATION ---
-  io.tx := core.io.tx
+  io.tx := core.io.tx*/
 }
 
 object Top extends App {
